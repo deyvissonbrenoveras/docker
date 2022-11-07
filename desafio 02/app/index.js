dc2b3adfaff0be7f3c8e75bc26403e73e3925771
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-11-07 19:54:35 -0300

    desafio 02 - app

---
+++ desafio 02/app/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const port = 3000;
+const app = express();
+const config = {
+  host: "db",
+  user: "root",
+  password: "password",
+  database: "nodedb",
+};
+const connection = mysql.createConnection(config);
+
+function savePerson(personName) {
+  connection.query(`INSERT INTO people(name) values('${personName}')`);
+}
+
+function getPeople(callback) {
+  connection.query(`SELECT * FROM people`, callback);
+}
+
+app.post("/", express.json(), async (req, res) => {
+  console.log(req.body.personName);
+  savePerson(req.body.personName);
+  getPeople(function (err, result, fields) {
+    if (err) return res.send(err);
+    let itemsList = "";
+    result.forEach((row) => (itemsList += `<li>${row.name}</li>`));
+    const nameList = `<ul>${itemsList}</ul>`;
+    return res.send("<h1>Full Cycle Rocks!</h1>" + nameList);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Running on port " + port);
+});
---
+++ desafio 02/db/scripts/init.sql
@@ -0,0 +1,6 @@
+use nodedb;
+
+CREATE TABLE people (
+  id int,
+  name varchar(255)
+);
